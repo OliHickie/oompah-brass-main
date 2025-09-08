69f9a8bbd8612d1d771fe074ccb41ce32483a8ec
OliHickie <OliHickie@users.noreply.github.com>
2025-09-08 15:28:28 +0000

    deploy: a54c30db90b86269d90839dda90f6743a58dd810

---
+++ assets/EducationView-BYG-Nw22.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,o as n}from"./index-Bw-aYBvh.js";const o={},s={class:"h-screen bg-green-500"};function t(r,_){return n(),c("div",s)}const i=e(o,[["render",t]]);export{i as default};
