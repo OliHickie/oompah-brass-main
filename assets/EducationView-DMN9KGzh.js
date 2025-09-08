93865d3ba349b831f2b3466dc9c1cb542f3d81f8
OliHickie <OliHickie@users.noreply.github.com>
2025-09-08 13:26:15 +0000

    deploy: cbf12a8219475d3c21bd0952cf680f5e86365916

---
+++ assets/EducationView-DMN9KGzh.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,o as n}from"./index-Dld0n66z.js";const o={},s={class:"h-screen bg-green-500"};function t(r,_){return n(),c("div",s)}const i=e(o,[["render",t]]);export{i as default};
--- assets/EducationView-b7NI2mV_.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as c,o as n}from"./index-iywafPwO.js";const o={},s={class:"h-screen bg-green-500"};function t(r,_){return n(),c("div",s)}const i=e(o,[["render",t]]);export{i as default};
