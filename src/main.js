4d85919d1b30490ecc5c3b80c18fa51bc6762ed6
Oli Hickie <oli.h@hotmail.co.uk>
2025-09-08 16:19:40 +0100

    Add 404

--- src/main.js
+++ src/main.js
@@ -8,4 +8,12 @@ const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+router.isReady().then(() => {
+  // Check if GitHub Pages redirected us with a ?redirect= path
+  const redirect = new URLSearchParams(window.location.search).get('redirect')
+  if (redirect) {
+    router.replace(redirect)
+  }
+  app.mount('#app')
+})
+
