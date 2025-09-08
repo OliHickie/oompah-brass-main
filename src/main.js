06a0b2e1eccfd38a43984c2f0b805fdb75f99665
Oli Hickie <oli.h@hotmail.co.uk>
2025-09-08 16:22:42 +0100

    Revert changes

--- src/main.js
+++ src/main.js
@@ -8,12 +8,4 @@ const app = createApp(App)
 
 app.use(router)
 
-router.isReady().then(() => {
-  // Check if GitHub Pages redirected us with a ?redirect= path
-  const redirect = new URLSearchParams(window.location.search).get('redirect')
-  if (redirect) {
-    router.replace(redirect)
-  }
-  app.mount('#app')
-})
-
+app.mount('#app')
